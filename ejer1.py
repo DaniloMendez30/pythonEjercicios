da4a0dc6e1d869d9e8ddb98eafbf048b3d84624a
DaniloMendez30 <106980383+DaniloMendez30@users.noreply.github.com>
2022-08-31 08:36:55 -0500

    Se empieza a desarrollar el ejercios de descuento

---
+++ ejer1.py
@@ -0,0 +1,35 @@
+def run():
+  total_factura = int(input("Introdusca la cantidad total de la compra: "))
+  descuento = [
+    {
+        'id': 0,
+        'color': 'Blanco',
+        'descuento': 0,
+    },
+    {
+        'id': 1,
+        'color': 'Roja',
+        'descuento': 0.10,
+    },
+    {
+        'id': 2,
+        'color': 'Azul',
+        'descuento': 0.2,
+    },
+    {
+        'id': 3,
+        'color': 'Verde',
+        'descuento': 0.25,
+    },
+    {
+        'id': 4,
+        'color': 'Amarilla',
+        'descuento': 0.5,
+    },
+              ]
+
+
+
+if __name__ == "__main__":
+  run()
+  
